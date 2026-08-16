4d7cae79f6d57fb1c15eda19cfc66be422059f25
Manish Joshi <manishjoshimannu2024@gmail.com>
2026-08-16 13:34:23 +0530

    Update documentRoutes.js

--- routes/documentRoutes.js
+++ routes/documentRoutes.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { exportDocument } = require("../controllers/exportController");
 
-router.post("/", exportDocument);
+const documentController = require("../controllers/documentController");
+const validate = require("../middleware/documentValidator");
+
+const auth = require("../middleware/auth");
+
+router.get("/", auth, documentController.list);
+router.post("/", auth, documentController.create);
+router.post("/import", auth, documentController.importDocument);
+router.get("/:id", auth, validate, documentController.findOne);
+router.put("/:id", auth, validate, documentController.update);
+router.post("/:id/duplicate", auth, validate, documentController.duplicate);
+router.delete("/:id", auth, validate, documentController.remove);
 
 module.exports = router;
